6049fe0aefbf3877fb3740b7407dc827b27969c2
Coder12a <38924418+Coder12a@users.noreply.github.com>
2019-03-15 23:52:17 -0500

    Fix: not going to right path

--- factions.lua
+++ factions.lua
@@ -3,10 +3,10 @@
 factions = {}
 
 -- Create cold databases.
-factions.factions = colddb.Colddb(minetest.get_worldpath() .. "factions/factions")
-factions.parcels = colddb.Colddb(minetest.get_worldpath() .. "factions/parcels")
-factions.players = colddb.Colddb(minetest.get_worldpath() .. "factions/players")
-factions.player_ips = colddb.Colddb(minetest.get_worldpath() .. "factions/ips")
+factions.factions = colddb.Colddb(minetest.get_worldpath() .. "/factions/factions")
+factions.parcels = colddb.Colddb(minetest.get_worldpath() .. "/factions/parcels")
+factions.players = colddb.Colddb(minetest.get_worldpath() .. "/factions/players")
+factions.player_ips = colddb.Colddb(minetest.get_worldpath() .. "/factions/ips")
 
 -- Memory only storage.
 factions.onlineplayers = {}
